20fdd55f50816837c2bcdcb0cad760afa0425e6d
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2026-04-14 22:36:41 -0500

    fix: performance

--- src/code/print_factors.py
+++ src/code/print_factors.py
@@ -1,6 +1,12 @@
+import math
+
+
 def get_factors(number: int) -> list[int]:
-    return [
-        i
-        for i in range(1, number + 1)
-        if number % i == 0
-    ]
+    factors = set()
+
+    for i in range(1, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            factors.add(i)
+            factors.add(number // i)
+
+    return sorted(list(factors))
